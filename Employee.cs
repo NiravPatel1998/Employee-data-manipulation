dbdb57516ef76358e91d5cc3898d034b43632328
Nirav Patel <44130575+NiravPatel1998@users.noreply.github.com>
2018-11-26 00:16:23 -0500

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,124 @@
+﻿/* Author - Nirav Patel
+ * Student ID - 000772459
+ * Statement of Authorship - I Nirav patel,000772459 had done this lab by my own and didn't copied anything without acknowledgement
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/// <summary>
+/// This class describes the employee
+///  This Employee class stores every single employee's data and return the values as per the objects are called in main method.
+/// </summary>
+namespace Lab4a
+{
+    class Employee : IComparable<Employee>
+    {
+        private decimal gross;        //to calculate gross
+        public double Hours { get; set; }    //hours of employee worked
+        public string Name { get; set; }   //name of employee
+        public int Number { get; set; }    //number of employee
+        public decimal Rate { get; set; } //rate of employee
+        public decimal Gross
+        {
+            get
+            {
+                double overTime;                // calculate overtime under the condition hours greater than 40
+                if (Hours > 40.0)
+                {
+                    overTime = Hours - 40.0;
+                    overTime = (double)(overTime / 2);
+                    overTime = overTime + Hours;
+                    gross = Rate * (decimal)overTime;
+                }
+                else
+                {
+                    gross = Rate * (decimal)Hours;
+                }
+                return gross;
+            }
+        }
+        public Employee()
+        {
+
+        }
+        /// <summary>
+        ///  Set the class varaibles value from the parameters passed in construction 
+        ///  and calculate gross pay as per given condition
+        /// </summary>
+        /// <param name="name">name of employee</param>
+        /// <param name="number">number of the employee</param>
+        /// <param name="rate">rate charge of the employee</param>
+        /// <param name="hours">hours the emmployee work</param>
+        public Employee(string name, int number, decimal rate, double hours)
+        {
+
+
+
+        }
+        /// <summary>
+        /// Print the formatted string contains data of employee.
+        /// </summary>
+        public void PrintEmployee()
+        {
+            Console.WriteLine($"{Name,-18} {Number,-10} {$"{Rate:C}",10} {$"{Hours:F}",10} {$"{Gross:C}",15}");
+        }
+        public static EmployeeComparer GetComparer()
+        {
+            return new EmployeeComparer();
+        }
+
+        public int CompareTo(Employee other, EmployeeComparer.ComparisonType which)
+        {
+            switch (which)
+            {
+                case EmployeeComparer.ComparisonType.Name:
+                    return this.Name.CompareTo(other.Name);
+                case EmployeeComparer.ComparisonType.Number:
+                    return this.Number.CompareTo(other.Number);
+                case EmployeeComparer.ComparisonType.Rate:
+                    return other.Rate.CompareTo(this.Rate);
+                case EmployeeComparer.ComparisonType.Hours:
+                    return other.Hours.CompareTo(this.Hours);
+                case EmployeeComparer.ComparisonType.Gross:
+                    return other.Gross.CompareTo(this.Gross);
+            }
+            return 0;
+
+        }
+        /// <summary>
+        /// compare two value of employee for sorting
+        /// </summary>
+        /// <param name="other">another employee</param>
+        /// <returns></returns>
+        public int CompareTo(Employee other)
+        {
+            return this.Name.CompareTo(other.Name);
+        }
+        /// <summary>
+        /// to get the type of comparison user want to sort
+        /// </summary>
+        public class EmployeeComparer : IComparer<Employee>
+        {
+            public EmployeeComparer.ComparisonType WhichComparison { get; set; }
+
+            public enum ComparisonType
+            {
+                Name,
+                Number,
+                Rate,
+                Hours,
+                Gross
+            };
+            public int Compare(Employee lhs, Employee rhs)
+            {
+                return lhs.CompareTo(rhs, WhichComparison);
+            }
+
+
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,138 @@
+﻿/* Author - Nirav Patel
+ * Student ID - 000772459
+ * Statement of Authorship - I Nirav patel,000772459 had done this lab by my own and didn't copied anything without acknowledgement
+ */
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// This lab sort the data of employees.txt file , the main class make an array of Employee type and store all the data of employees
+/// in it. There are two methods one read the file and another sort the data as per user input choice.
+///
+namespace Lab4a
+{
+    class Program
+    {
+        /// <summary>
+        /// 
+        /// The main method creates an array objects of Employee type and stores the data of all employees and take user input.
+        /// It also calls Read() and Sort() method and then print the sorted array
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+
+            List<Employee> employees = new List<Employee>();
+            int count = Read(employees);                            // size of array
+            //this while loop will be continue unless the user will enter the exit option.
+            while (true)
+            {
+                Console.WriteLine("1. Sort by Employee Name (ascending)\n2. Sort by Employee Number(ascending)\n3. Sort by Employee Pay Rate(descending)\n4. Sort by Employee Hours(descending)\n5. Sort by Employee Gross Pay(descending)\n6. Exit");
+                Console.WriteLine("Enter your choice: ");
+                int choice = int.Parse(Console.ReadLine());         // choice of sorting
+                if (choice == 6)
+                {
+                    break;
+                }
+                else if (choice > 6 || choice < 1)
+                {
+                    Console.WriteLine("-------Please enter valid choice given above-------\n");
+                }
+                else
+                {
+                    Sort(choice, employees, count);
+                    Console.WriteLine("------------------------------------------------------------------------");
+                    Console.WriteLine($"{"Name",-18} {"Number",-15} {"Rate",-10} {"Hours",-12} {"Gross Pay",-15}");
+                    Console.WriteLine("------------------------------------------------------------------------");
+                    foreach (Employee e in employees)
+                    {
+                        e.PrintEmployee();
+                    }
+                    Console.WriteLine("------------------------------------------------------------------------");
+                }
+            }
+        }
+        /// <summary>
+        /// Read() method read the employees.txt file and store the data in created Employee's object 
+        /// </summary>
+        /// <param name="employees"> it is an array of Employee type stores each employee's data at respective index</param>
+        /// <returns>the number of employees or the size of array</returns>
+        static int Read(List<Employee> employees)
+        {
+            //to read the file "employees.tx" 
+            int count = 0;
+            try
+            {
+                FileStream inputFile = new FileStream("employees.txt", mode: FileMode.Open, access: FileAccess.Read);
+                StreamReader employeeData = new StreamReader(inputFile);
+                string oneEmployeeData = String.Empty;                           // stores single line of file.
+                while ((oneEmployeeData = employeeData.ReadLine()) != null)
+                {
+                    string[] spiltedData = oneEmployeeData.Split(',');          //takes one line data and spilt at "," then stores in this string array
+                    Employee e = new Employee();
+                    e.Name = spiltedData[0];
+                    e.Number = int.Parse(spiltedData[1]);
+                    e.Rate = decimal.Parse(spiltedData[2]);
+                    e.Hours = double.Parse(spiltedData[3]);
+
+                    employees.Add(e);
+                    count++;
+                }
+                employeeData.Close();
+                inputFile.Close();
+
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            return count;
+        }
+        /// <summary>
+        /// Sort() method sort the array as per the user choice.
+        /// This method use Selection Sort method. 
+        /// "I had taken the alogrithm of selection sort from my another course - Data Structure and Alogrithm's given Examples"
+        /// </summary>
+        /// <param name="choice">the user input choice</param>
+        /// <param name="employees">array of Employee type which have employee's data</param>
+        /// <param name="count">size of array</param>
+        static void Sort(int choice, List<Employee> employees, int count)
+        {
+            Employee.EmployeeComparer e = Employee.GetComparer();
+            switch (choice)
+            {
+                //--------------- to sort by name of employee------------------//
+                case 1:
+                    e.WhichComparison = Employee.EmployeeComparer.ComparisonType.Name;
+                    employees.Sort(e);
+                    break;
+                //-------------- to sort by number of employee-----------------//
+                case 2:
+                    e.WhichComparison = Employee.EmployeeComparer.ComparisonType.Number;
+                    employees.Sort(e);
+                    break;
+                //------------ to sort by rate of employee-------------------//
+                case 3:
+                    e.WhichComparison = Employee.EmployeeComparer.ComparisonType.Rate;
+                    employees.Sort(e);
+                    break;
+                //------------- to sort by hours of the employee work -------//
+                case 4:
+                    e.WhichComparison = Employee.EmployeeComparer.ComparisonType.Hours;
+                    employees.Sort(e);
+                    break;
+                //------------- to sort by gross pay of the employee -------//
+                case 5:
+                    e.WhichComparison = Employee.EmployeeComparer.ComparisonType.Gross;
+                    employees.Sort(e);
+                    break;
+                default:
+                    break;
+
+            }
+        }
+    }
+}
